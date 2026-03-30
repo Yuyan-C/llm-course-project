2ff43b2c6caf9191fb01a7589178f8daae728a82
Yuyan-C <yuyan.chen2@mail.mcgill.ca>
2026-03-30 14:35:57 -0400

    debug: change grounding dino output type

--- src/tools/object_detection/grounding_dino.py
+++ src/tools/object_detection/grounding_dino.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, List
 
 def run_grounding_dino(image_path: str, text_labels: List) -> Dict[str, Any]:
     """
-    Run Grounding DINO object detection on the given image and text labels.
+    An object detection model to detect instances of the specified text labels in the input image. It uses the GroundingDINO model for object detection.
     Args:
         image_path: The path to the input image.
         text_labels: The text labels to be used for object detection.
@@ -28,17 +28,27 @@ def run_grounding_dino(image_path: str, text_labels: List) -> Dict[str, Any]:
         outputs,
         inputs.input_ids,
         threshold=0.3,
-        text_threshold=0.6,
+        text_threshold=0.8,
         target_sizes=[image.size[::-1]]
     )
 
     result = results[0]
 
+    result['scores'] = result['scores'].cpu().numpy().tolist()
+    result['boxes'] = result['boxes'].cpu().numpy().tolist()
+
     return result
 
 
-   
-   
+if __name__ == "__main__":
+    
 
+    image_path = "/network/scratch/y/yuyan.chen/inquire/train/00261_Animalia_Arthropoda_Insecta_Coleoptera_Cerambycidae_Typocerus_velutinus/4e9c98d9-1fcd-41f3-a1e3-f206982e0210.jpg"
+
+    text_labels = [["an insect", "a white flower","a bear"]]
+    result = run_grounding_dino(image_path, text_labels)
+
+    print(result)
+  
 
 
